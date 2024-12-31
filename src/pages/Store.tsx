e97019193946cd23097b729ee8cf14d09e5a3dc6
craigles <craig.d.curtis@gmail.com>
2024-12-31 16:11:29 +1000

    change to port 8080

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -13,7 +13,7 @@ const ProductDisplay = () => (
         <h5>$30.00</h5>
       </div>
     </div>
-    <form action={`https://plt-cnh3fzhghnesfsby.australiaeast-01.azurewebsites.net:4242/create-checkout-session`} method="POST">
+    <form action={`https://plt-cnh3fzhghnesfsby.australiaeast-01.azurewebsites.net:8080/create-checkout-session`} method="POST">
       <button type="submit">
         Checkout
       </button>
