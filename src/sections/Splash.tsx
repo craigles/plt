afcf86c41e300211e0f673f5a67ba5c8eb15d02f
craigles <craig.d.curtis@gmail.com>
2025-05-01 13:09:59 +1000

    Update splash

--- src/sections/Splash.tsx
+++ src/sections/Splash.tsx
@@ -3,7 +3,7 @@ export const Splash: React.FC = () => {
         <div className="splash">
             <p>Lead single <i>Junk!</i> from debut album dropping May 2nd</p>
             <br/>
-            <a href="https://gyro.to/Junk" className="">
+            <a href="https://plt.lnk.to/junk" className="">
                 <img width={550} src="../images/albums/Junk.jpg" />
             </a>
         </div>
