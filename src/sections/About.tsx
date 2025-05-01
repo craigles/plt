4b28519f2e1340c61bcf6ef1c382fe26fbd25f46
craigles <craig.d.curtis@gmail.com>
2025-05-01 20:30:53 +1000

    Update bio

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -4,12 +4,10 @@ export const About: React.FC = () => {
     return (
         <div className={styles.about}>
             <h1><a id="about" href="/#"></a>About</h1>
-            <p>Preloved Things were regurgitated from the very bottom of the rabbit hole. They sound like a dream-tornado obliterated the local 7-11 and showered your neighbourhood in candy. While you sunbathe on the grassy knoll, they are the lone hailstone which strikes upon your nuts.</p> 
-            <p>Preloved Things are dynamical, maniacal, poetical, a little political. Brittle Guitar breaks and confounding arrangements. Furious psychedelica yet moments of wafer thin subtlety. Drumming like a cat fight in a china cabinet. Lyrics like sublime  trickery.</p>
-            <p>The band emerged from the culturally sterile Brisbane suburbs and have only recently made first contact with the outside world. Scientists are studying their sound to see what medicinal properties it may contain and if it could be leveraged profitably on the open market.</p>
-            <p>Songs titles like <i>The Cats Are Building Mouse Holes</i> and <i>Reset the Internet</i> or <i>K8lyn (with an 8 in it)</i> foreshadow some serious lyrical clout.  </p>
-            <p>Their live show is like an organism that became sentient when lightning struck a bar fight and a philosophy lecture at the same time. Preloved Things can temporarily interrupt the iPhone codependence of jaded millennials in the audience.</p>
-            <p>With their debut long player <i>How Life Strange Is</i> dropping on wax in 2025, Preloved Things are poised to do more and bigger and beautifuller.</p>
+            <p>Preloved Things were regurgitated from the very bottom of the rabbit hole. They are dynamical, maniacal, poetical, a little political. This art-rock group from Brisbane play meticulously constructed songs that charm, challenge and confound.</p> 
+            <p>In 2018 they released their debut EP <i>Kitchen Sink Drama</i>. They won best music video at the West End Film Festival that year for their song <i>Entertaining the Thought (of Leaving You)</i>.</p>
+            <p>Their live shows are like an organism that became sentient after lightning struck a bar fight and a philosophy lecture at the same time. Rumor has it, Preloved Things have the power to temporarily interrupt the iPhone co-dependence of jaded millennials and Gen Z'ers. No mean feat.</p>
+            <p><i>"I would hurdle the fiery sinkholes to hell, to see this art-rock chef-d'oeuvre played live."</i> - Brisbane Indie Gems</p>
         </div>
     );
 }
