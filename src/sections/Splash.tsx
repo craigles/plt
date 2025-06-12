ca13db3c2090668ba60a24c7a9ac78354903e43a
Craig Curtis <craig.d.curtis@gmail.com>
2025-06-12 21:25:56 +1000

    Update Splash.tsx

--- src/sections/Splash.tsx
+++ src/sections/Splash.tsx
@@ -1,10 +1,10 @@
 export const Splash: React.FC = () => {
     return (
         <div className="splash">
-            <p><i>Junk!</i> is the lead single from our debut album <i>How Life Strange Is</i>. Listen now!</p>
+            <p>Our debut album <i>How Life Strange Is</i> is out now!</p>
             <br/>
-            <a href="https://plt.lnk.to/junk" className="">
-                <img width={550} src="../images/albums/Junk.jpg" />
+            <a href="https://plt.lnk.to/howlifestrangeis" className="">
+                <img width={550} src="../images/albums/How Life Strange Is.jpg" />
             </a>
         </div>
     );
