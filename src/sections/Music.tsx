6b5cdb22691b10d1d64f66a3f4375c8eb1654172
craigles <craig.d.curtis@gmail.com>
2025-07-10 08:20:38 +1000

    Adding music and videos

--- src/sections/Music.tsx
+++ src/sections/Music.tsx
@@ -1,8 +1,12 @@
+import styles from './Music.module.scss';
+
 export const Music: React.FC = () => {
     return (
-        <div className="music">
+        <div className={styles.music}>
             <h1><a id="music" href="/#"></a>Music</h1>
-            <iframe style={{ "border": 0, "width": "350px", "height": "350px" }} src="https://bandcamp.com/EmbeddedPlayer/album=1355514861/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="https://prelovedthings.bandcamp.com/album/kitchen-sink-drama">Kitchen Sink Drama by Preloved Things</a></iframe>
+                <iframe style={{ "border": 0, "width": "350px", "height": "350px" }} src="https://bandcamp.com/EmbeddedPlayer/album=1706571441/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="https://prelovedthings.bandcamp.com/album/how-life-strange-is">How Life Strange Is by Preloved Things</a></iframe>
+
+                <iframe style={{ "border": 0, "width": "350px", "height": "350px" }} src="https://bandcamp.com/EmbeddedPlayer/album=1355514861/size=large/bgcol=ffffff/linkcol=0687f5/minimal=true/transparent=true/" seamless><a href="https://prelovedthings.bandcamp.com/album/kitchen-sink-drama">Kitchen Sink Drama by Preloved Things</a></iframe>
         </div>
     );
 }
--- src/sections/Videos.tsx
+++ src/sections/Videos.tsx
@@ -4,6 +4,8 @@ export const Videos: React.FC = () => {
     return (
         <div className="videos">
             <h1><a id="videos" href="/#"></a>Videos</h1>
+            <YouTubeVideo src="https://www.youtube.com/embed/tYUdB7D0HIA?si=UuY1nSgoAbphd-QA" />
+            <hr />
             <YouTubeVideo src="https://www.youtube.com/embed/dLys1Mm2yBY?si=67S8CX36ZU9dPBUj" />
             <hr />
             <YouTubeVideo src="https://www.youtube.com/embed/3upqlxFrSBE?si=EpBFL4AWkesE7mjZ" />
