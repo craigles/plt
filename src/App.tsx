d09495cc59cad0eae30794fb2d83395962d159fd
craigles <craig.d.curtis@gmail.com>
2025-10-03 18:04:24 +1000

    Remove store

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.scss';
 import "react-image-gallery/styles/scss/image-gallery.scss";
 import { Header, Footer } from './sections';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { Home, Store } from './pages';
+import { Home } from './pages';
 
 function App() {
   return (
@@ -11,7 +11,6 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/store" element={<Store />} />
           <Route path='*' element={<Navigate to="/" replace />} />
         </Routes>
       </main>
